f519a8d4dba021f99435f190803e009be128fc75
Rishad1383 <nizamuddin1383@gmail.com>
2023-04-27 00:22:27 +0530

    animatining

---
+++ index.js
@@ -0,0 +1,34 @@
+function getRndInteger(min, max) {
+  var yourName = prompt("What is your name?");
+  prompt("What is your lover's name");
+
+  var loveScore = Math.random() * 100;
+  loveScore = Math.floor(loveScore) + 1;
+  if (loveScore > 85) {
+    alert(
+      "Your love Score is " +
+        loveScore +
+        "%,😮" +
+        yourName +
+        "!! it meane that you love each other very much!💞💝💖💕"
+    );
+  }
+  if (loveScore > 30 && loveScore <= 85) {
+    alert(
+      "Your love Score is " +
+        loveScore +
+        "%, Good, but you have to work on your relationship " +
+        yourName +
+        "!💛😑"
+    );
+  }
+  if (loveScore <= 30) {
+    alert(
+      "your loveScore is " +
+        loveScore +
+        "%, 🤭😆 Poor " +
+        yourName +
+        "!, your love is falled don't try🤣"
+    );
+  }
+}
